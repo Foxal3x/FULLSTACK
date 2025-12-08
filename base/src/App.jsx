c8c63417a9d3396a6212d187d0c86b5617954967
Foxal3x <alexander.t.hogue@gmail.com>
2025-12-08 10:35:15 -0600

    adding phonebook stuffs

--- base/src/App.jsx
+++ base/src/App.jsx
@@ -1,12 +1,31 @@
-const App = ({ notes }) => {
+import React from 'react'
+import{ useState,useEffect} from 'react'
+import PersonForm from './components/PersonForm'
+
+const App = () => {
+  const [ persons, setPersons] = useState([])
+  const [ allPersons, setAllPersons] = useState([])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+
+  const handleNameChange =(e)=>{
+    setNewName(e.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const addPerson=(e)=>{
+    e.preventDefault()
+    console.log("ADDING PERSON")
+    const personToAdd={name:newName,number:newNumber}
+  }
+
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
+      <h2>Phonebook</h2>
+      <PersonForm onSubmit={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
+      <h2>Numbers</h2>
+      ...
     </div>
   )
 }
